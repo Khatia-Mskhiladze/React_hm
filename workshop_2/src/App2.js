39f7ee3f78e50447110bf3a72ad430cd6fb0ace0
Khatia-Mskhiladze <123637463+Khatia-Mskhiladze@users.noreply.github.com>
2023-10-31 20:54:05 +0400

    add changed App2 file

--- workshop_2/src/App2.js
+++ workshop_2/src/App2.js
@@ -3,7 +3,7 @@ import { Container} from './Components/Container';
     return (
         <section className="box_container" >
             <Container />
-            <img alt="photo" src="https://www.thedesignwork.com/wp-content/uploads/2011/10/Random-Pictures-of-Conceptual-and-Creative-Ideas-01.jpg" />
+            <img alt="cover" src="https://www.thedesignwork.com/wp-content/uploads/2011/10/Random-Pictures-of-Conceptual-and-Creative-Ideas-01.jpg" />
         </section>
     )
 }
