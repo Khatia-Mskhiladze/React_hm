8302cb90fe4fe392dc13d51e8e3530b1bcde34ce
GioDziko <gdzir2018@agruni.edu.ge>
2023-11-11 00:19:32 +0400

    fix problems

--- workshop_5/src/pages/Universities/Universities.js
+++ workshop_5/src/pages/Universities/Universities.js
@@ -1,59 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import { FadeLoader } from 'react-spinners';
-import University from '../../components/University';
-
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+import { FadeLoader } from "react-spinners";
+import University from "../../components/University";
 
 const Universities = () => {
+  const [universities, setUniversities] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
 
-    const [universities, setUniversities] = useState([]);
-    const [error, setError] = useState("");
-    const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    fetch("http://universities.hipolabs.com/search?country=Georgia")
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error("error");
+      })
 
+      .then((data) => {
+        setUniversities(data);
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch("http://universities.hipolabs.com").then((res) => {
-            if (res.ok) {
-                return res.json();
-            }
-        })
-        throw new Error("error")
-
-            .then((data) => {
-                setUniversities(data);
-                console.log(data);
-            })
-            .catch((error) => {
-                setError(error.message);
-            })
-            .finally(() => {
-                setIsLoading(false);
-            })
-    }, []);
-
-
-    if (error) {
-        return (
-            <div>
-                <h1>Oops... There is a problem</h1>
-            </div>
-        )
-    }
-
+  if (error) {
     return (
-        <div>
+      <div>
+        <h1>Oops... There is a problem</h1>
+      </div>
+    );
+  }
 
-            <h1>Hello</h1>
-            {isLoading && <FadeLoader />}
+  return (
+    <div>
+      <h1>Hello</h1>
+      {isLoading && <FadeLoader />}
 
-            {universities.map((item)=> {
-                <University data={item} key={item.website}/>
-            })}
-
-       
-
-
-        </div>
-    )
-}
+      {universities.map((item) => (
+        <University data={item} key={item.name} />
+      ))}
+    </div>
+  );
+};
 
 export default Universities;
